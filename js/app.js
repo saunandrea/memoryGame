9598580918be0d2583f265449c5a6370f0a383dd
Andrea Saunders <saunandrea@gmail.com>
2018-06-24 14:22:00 -0600

    save a checkpoint, trblshoot timer not resetting

--- js/app.js
+++ js/app.js
@@ -13,6 +13,12 @@ const cards = [
 ];
 
 let openCard = null;
+let timeoutComplete = true;
+let matchCounter = 0;
+let attempts = 0;
+const moves = document.getElementsByClassName("moves")[0];
+let startDate;
+var timer; 
 
 /*
  * Display the cards on the page
@@ -48,40 +54,110 @@ function shuffle(array) {
     return array;
 }
 
-function setupEvents() {
-    document.getElementsByClassName("deck")[0].addEventListener("click", function (e) {
-        // e.target is the clicked element!
-        // If it was a list item
-        if (e.target && e.target.nodeName == "LI" && !e.target.classList.contains("open") && !e.target.classList.contains("match")) {
-            // List item found!  Output the ID!
-            let iconElement = e.target.firstChild;
-            e.target.classList.add("open");
-            console.log("List item ", iconElement.classList, " was clicked!");
-            let iconClass;
-            for (let c of iconElement.classList) {
-                if (c.indexOf('fa-') > -1) {
-                    iconClass = c;
-                }
-            }
-            if (openCard) {
-                if (openCard.firstChild.classList.contains(iconClass)) {
-                    console.log("it's a match");
-                    e.target.classList.toggle("match");
-                    openCard.classList.toggle("match");
-                    e.target.classList.toggle("open");
-                    openCard.classList.toggle("open");
-                    openCard = null;
-                } else {
-                    console.log("not looking good");
-                    e.target.classList.toggle("open");
-                    openCard.classList.toggle("open");
-                    openCard = null;
-                }
-
-            } else {
-                openCard = e.target;
+function checkifCardsMatch(lastClicked) {
+    if (openCard) {
+        let iconElement = lastClicked.firstChild;
+        console.log("List item ", iconElement.classList, " was clicked!");
+        let iconClass;
+        for (let c of iconElement.classList) {
+            if (c.indexOf('fa-') > -1) {
+                iconClass = c;
             }
         }
+        if (!iconClass) {
+            console.log("Error. Not an icon.");
+            return;
+        }
+        if (openCard.firstChild.classList.contains(iconClass)) {
+            console.log("it's a match");
+            lastClicked.classList.toggle("match");
+            openCard.classList.toggle("match");
+            matchCounter++;
+            if (matchCounter == 8) {
+                console.log("you woooonnn!!!!");
+                resetGame();
+                return;
+            }
+        } else {
+            console.log("not looking good");
+            lastClicked.classList.toggle("open");
+            openCard.classList.toggle("open");
+        }
+        openCard = null;
+        setAttempts(attempts + 1);
+    } else {
+        if(attempts == 0 ){
+            console.log("want timer to start");
+            startDate = new Date().getTime();
+            timer = window.setInterval(function () {
+                var now = new Date().getTime();
+                var distance =  now - startDate;
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+            }, 1000);
+        }
+        openCard = lastClicked;
+    }
+
+    /*     if (!timeoutComplete) {
+            setTimeout(function () {
+                timeoutComplete = true;
+            }, 1000)
+        } */
+
+}
+
+function setAttempts(newVal) {
+    attempts = newVal;
+    if (attempts == 15) {
+        document.getElementsByClassName("stars")[0].firstElementChild.style.display = "none";
+    }
+    else if (attempts == 20) {
+        document.getElementsByClassName("stars")[0].lastElementChild.style.display = "none";
+    }
+    moves.innerHTML = attempts;
+}
+
+function resetGame() {
+    window.clearInterval(timer);
+    timer = null; //todo: for some reason timer not starting again after a reset
+    document.getElementById("timer").innerHTML = "0m 0s ";
+    matchCounter = 0;
+    setAttempts(0);
+    //todo: store a variable with this list so not looking up so much 
+    document.getElementsByClassName("stars")[0].children[0].style.display = "inline";
+    document.getElementsByClassName("stars")[0].children[2].style.display = "inline";
+    //todo: ideally would like to sub out the classes but can optimize in a rev 2 vs deleting and readding
+    let deck = document.getElementsByClassName("deck")[0];
+    while (deck.firstChild) {
+        deck.removeChild(deck.firstChild);
+    }
+    setCards(cards);
+
+}
+
+function setupEvents() {
+    document.getElementsByClassName("deck")[0].addEventListener("click", function (e) {
+        let target;
+        if (e.target) {
+            target = e.target;
+        } else {
+            return;
+        }
+        if (target.nodeName == "I") {
+            target = target.parentNode;
+        }
+        if (target && (target.nodeName == "LI") && !target.classList.contains("open")) {  //(timeoutComplete || !openCard) &&
+            target.classList.add("open");
+            setTimeout(function () {
+                checkifCardsMatch(target);
+            }, 1000)
+        }
+    });
+
+    document.getElementsByClassName("restart")[0].addEventListener("click", function (e) {
+        resetGame();
     });
 }
 
